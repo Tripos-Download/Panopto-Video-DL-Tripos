16f99029239904dc5d1b4f09e8abbcd9b72e4a9c
Tripos-Download <triposdownload@gmail.com>
2025-06-14 13:36:13 +0100

    Initial Commit
    Fix button in list view.
    Copy link only.
    Style the button to match panopto.

--- Panopto-Video-DL-Tripos.js
+++ Panopto-Video-DL-Tripos.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Panopto-Video-DL
-// @namespace    https://github.com/Panopto-Video-DL
+// @name         Panopto-Video-DL-Tripos
+// @namespace    https://github.com/Panopto-Video-DL-Tripos
 // @description  Video downloader for Panopto
 // @icon         https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://panopto.com&size=96
-// @author       Panopto-Video-DL
-// @version      3.5.1
+// @author       Panopto-Video-DL, Tripos-Download
+// @version      1.0
 // @copyright    2021, Panopto-Video-DL
 // @license      MIT
 // @homepage     https://github.com/Panopto-Video-DL/Panopto-Video-DL-browser
@@ -34,84 +34,98 @@
   addStyle('#Panopto-Video-DL{position:fixed;top:10%;left:50%;width:70%;padding:2em 3em 1em;background-color:#2d3436;transform:translateX(-50%);z-index:1050}#Panopto-Video-DL *{margin-bottom:10px;color:#fff!important;font-size:18px;}#Panopto-Video-DL > div {margin-top: 1em;}#Panopto-Video-DL ul,#Panopto-Video-DL ol,#Panopto-Video-DL li{margin:0 .5em;padding:0 .5em;list-style:decimal}#Panopto-Video-DL button{margin-left:5px;margin-right:5px;color:#000!important;font-size:16px;}#Panopto-Video-DL p{margin-top:0.5em;}#Panopto-Video-DL input{color:black!important;}#Panopto-Video-DL textarea{width:100%;color:black!important;resize:vertical;white-space:nowrap;}')
 
   if (location.pathname.includes('/List.aspx')) {
-    log('Service started');
+    $(document).ready(function() {
+      if (location.pathname.includes('/List.aspx')) {
+        log('Service started');
 
-    const button = document.createElement('button');
-    button.className = 'css-t83cx2 css-tr3oo4 css-coghg4';
-    button.role = 'button';
-    button.style.marginLeft = '0.5rem';
-    button.innerHTML = '<span class="material-icons css-6xugel" style="font-size: 18px;margin-bottom:-0.25rem;">file_download</span>Download';
+        const button = document.createElement('button');
+        button.className = 'MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButton-colorPrimary MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButton-colorPrimary sort-menu-button css-ps1z9a';
+        button.role = 'button';
+        button.style.marginLeft = '16px';
+        button.innerHTML = 'Get Links';
 
-    button.addEventListener('click', e => {
-      e.preventDefault();
-      e.stopPropagation();
+        button.addEventListener('click', e => {
+          e.preventDefault();
+          e.stopPropagation();
 
-      let _t;
-      const list = (_t = document.querySelectorAll('#listViewContainer tbody > tr a.detail-title')).length ?
-            _t : (_t = document.querySelectorAll('#detailsTable tbody > tr a.detail-title')).length ?
-            _t : (_t = document.querySelectorAll('#thumbnailGrid > li a.detail-title')).length ?
-            _t : null;
-      if (!list) {
-        log('No videos found', 'error');
-        new Notify({
-          text: 'No videos found',
-          type: 'error'
-        }).show();
-        return;
-      }
-
-      const n = new Notify({
-        text: 'Getting links. Please wait',
-        type: 'info',
-        timeout: 2000
-      });
-      n.show();
-
-      const requestsList = [...list].map(item => {
-        let videoId = new URL(item.getAttribute('href')).searchParams.get('id');
-        const videoTitle = item.textContent.trim();
-        return requestDeliveryInfo(videoId)
-          .catch(error => {
+          let _t;
+          const list = (_t = document.querySelectorAll('#listViewContainer tbody > tr a.detail-title')).length ?
+                _t : (_t = document.querySelectorAll('#detailsTable tbody > tr a.detail-title')).length ?
+                _t : (_t = document.querySelectorAll('#thumbnailGrid > li a.detail-title')).length ?
+                _t : null;
+          if (!list) {
+            log('No videos found', 'error');
             new Notify({
-              text: 'Failed to get lesson link for "' + videoTitle + '"',
-              type: 'error',
-              timeout: null
+              text: 'No videos found',
+              type: 'error'
             }).show();
-          });
-      });
+            return;
+          }
 
-      Promise.allSettled(requestsList)
-        .then(responses => {
-          // log(responses)
-          n.close();
-          let copyText = '';
-          responses.forEach(response => {
-            if (response.status == 'fulfilled' && response.value) {
-              const streamUrl = response.value?.[0];
-              if (streamUrl)
-                copyText += streamUrl + '\n';
-            }
+          const n = new Notify({
+            text: 'Getting links. Please wait',
+            type: 'info',
+            timeout: 2000
           });
-          if (copyText !== '')
-            copyToClipboard(copyText);
+          n.show();
+
+          const requestsList = [...list].map(item => {
+            let videoId = new URL(item.getAttribute('href')).searchParams.get('id');
+            const videoTitle = item.textContent.trim();
+            return requestDeliveryInfo(videoId)
+              .catch(error => {
+                new Notify({
+                  text: 'Failed to get lesson link for "' + videoTitle + '"',
+                  type: 'error',
+                  timeout: null
+                }).show();
+              });
+          });
+
+          Promise.allSettled(requestsList)
+            .then(responses => {
+              n.close();
+              let copyText = '';
+              responses.forEach(response => {
+                if (response.status == 'fulfilled' && response.value) {
+                  const streams = response.value?.[1];
+                  const streamUrl = response.value?.[0];
+                  copyText += 'Audio\n' + streamUrl + '\n';
+                  streams.forEach((value, index) => {
+                    if (!(/Logo*./).test(value.Name)) {
+                      copyText += value.Name + '\n';
+                      copyText += value.StreamUrl + '\n';
+                    }
+                  })
+                }
+              });
+              if (copyText !== '') {
+                copyText = copyText.substring(0, copyText.length - 1)
+                copyToClipboard(copyText);
+              }
+              else {
+                new Notify({
+                  text: 'No Links Found.',
+                  type: 'error',
+                  timeout: 2000
+                }).show();
+              }
+            });
         });
+        document.querySelectorAll('#actionHeader button')[1]?.parentElement.appendChild(button);
+      }
     });
-
-    document.querySelector('#actionHeader button')?.parentElement.appendChild(button);
   }
   else if (location.pathname.includes('/Viewer.aspx')) {
-    log('Service started');
-
     const button = document.createElement('a');
     button.href = '#';
-    button.innerHTML = '<span class="material-icons" style="font-size:15px;margin-bottom:-0.25rem;">file_download</span> Download';
+    button.innerHTML = 'Get Links';
     button.classList = 'event-tab-header';
     button.style = 'display:inline-flex;align-items:center;position:absolute;bottom:30px;padding:5px 10px;text-decoration:none;cursor:pointer;';
 
     button.addEventListener('click', e => {
       e.preventDefault();
       e.stopPropagation();
-
       getVideoDownloadLink();
     });
     document.querySelector('#eventTabControl').appendChild(button);
@@ -129,7 +143,6 @@
     button.addEventListener('click', e => {
       e.preventDefault();
       e.stopPropagation();
-
       getVideoDownloadLink();
     });
 
@@ -170,28 +183,23 @@
         .then(_streams => {
           const streamUrl = _streams[0];
           const streams = _streams[1];
-
-          if (streamUrl.endsWith('master.m3u8') || streamUrl.match(/\.panobf\d+/)) {
-            if (localStorage.getItem('popup-viewed') != 'true')
-              showModal('<h1 style="text-align:center;font-size:30px;">READ ME</h1> <p>To download the video follow these steps:</p> <ol><li>Download this program from <a href="https://github.com/Panopto-Video-DL/Panopto-Video-DL" target="_blank">GitHub</a> (No installation needed) and open it</li> <li>Paste the automatically copied link</li> <li>Set the destination folder</li> <li>Wait for the download to finish</li> </ol> <p style="text-align:center;"> <button onclick="this.parentElement.parentElement.remove();">Close</button> <button onclick="localStorage.setItem(\'popup-viewed\', true);this.parentElement.parentElement.remove();">Close and don\'t show again</button> </p>');
-
-            copyToClipboard(streamUrl);
+          let copyText = 'Audio\n' + streamUrl + '\n';
+          streams.forEach((value, index) => {
+            if (!(/Logo*./).test(value.Name)) {
+              copyText += value.Name + '\n';
+              copyText += value.StreamUrl + '\n';
+            }
+          })
+          if (copyText !== '') {
+            copyText = copyText.substring(0, copyText.length - 1)
+            copyToClipboard(copyText);
           }
           else {
-            if (typeof GM_openInTab !== 'undefined')
-              GM_openInTab(streamUrl, false);
-            else
-              window.open(streamUrl);
-          }
-
-          if (streams.length && localStorage.getItem('other-source-viewed') != 'true') {
-            const modal = showModal('<h2 style="font-size:20px;">Download another source video</h2><ul></ul><p style="text-align:center;"><button onclick="this.parentElement.parentElement.remove();">Close</button><button onclick="localStorage.setItem(\'other-source-viewed\', true);this.parentElement.parentElement.remove();">Close and don\'t show again</button></p>');
-            streams.forEach((value, index) => {
-              const li = document.createElement('li');
-              li.innerHTML = (value.Name?.replace(/-?(\d{8}T\d+Z)+((.)?(\w+))?/g, '').replace(/_/g, ' ') || 'Stream ' + (index + 1)) + '<button>Copy</button>';
-              li.querySelector('button').addEventListener('click', (e) => { copyToClipboard(value.StreamUrl); })
-              modal.querySelector('ul').appendChild(li);
-            });
+            new Notify({
+              text: 'No Links Found.',
+              type: 'error',
+              timeout: 2000
+            }).show();
           }
         })
         .catch(error => {
